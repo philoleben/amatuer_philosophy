e990cc2c4965d88f789f54895e557e398b476cf6
DESKTOP-RHQTACD\User <philoleben@gmail.com>
2022-05-26 18:00:51 +0900

    models.py에 카테고리, 북, 북쉘프 테이블 모델 추가

--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -10,3 +10,22 @@ class User(db.Model):
     genre = db.Column(db.String(64))
     audio = db.Column(db.Integer)
     
+class Categories(db.Model): 
+    __tablename__ = 'categories'   #테이블 이름 : bookshelf
+    categoryid = db.Column(db.Integer, primary_key = True)   #id를 프라이머리키로 설정
+    name = db.Column(db.text())
+        
+class Book(db.Model):
+    __tablename__ = 'book'   #테이블 이름 : user
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text())
+    image = db.Column(db.Text())
+    categoryid = db.Column(db.Integer(8), db.ForeignKey('categories.id', ondelete='CASCADE'))
+            
+
+class Bookshelf(db.Model): 
+    __tablename__ = 'bookshelf'   #테이블 이름 : bookshelf
+    userid = db.Column(db.Integer(8), db.ForeignKey('user.id', ondelete='CASCADE'))
+    bookid = db.Column(db.Integer(8), db.ForeignKey('book.id', ondelete='CASCADE'))
