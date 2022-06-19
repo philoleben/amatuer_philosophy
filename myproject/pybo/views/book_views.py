843d2306a93f5e2a699f476cd3de217828db3f42
박주혁 <fqdsa123@gmail.com>
2022-06-19 11:14:43 +0900

    일부 에러 수정

--- myproject/pybo/views/book_views.py
+++ myproject/pybo/views/book_views.py
@@ -23,25 +23,26 @@ def details():
     book_id = int(request.args.get('bookId'))
     book = json_data[book_id]
     print(book_id)
+    
+    desc_list = book['description']
 
     author = book['authors']
     print(author)
-    this_authors_otherbooks = []
     
-    same_subject = []
-    for books in json_data:
-        for book_sub in book['subject']:
-            if book_sub in books['subjects']:
-                same_subject.append(books)
-                break
-
+    this_authors_otherbooks = []
     for _book in json_data:
         if _book['authors'] == author:
             this_authors_otherbooks.append(_book)
-            
+    
+    same_subject = []
+    for books in json_data:
+        for book_sub in book['subjects']:
+            if book_sub in books['subjects']:
+                same_subject.append(books)
+                break
     # same subjects or Doc2vec ?        
         
-    return render_template('book/details.html', book=book, this_authors_otherbooks=this_authors_otherbooks)
+    return render_template('book/details.html', book=book, this_authors_otherbooks=this_authors_otherbooks, desc_list=desc_list)
 
 @bp.route('/search/')
 def search():
@@ -64,8 +65,8 @@ def search():
 
         for idx in range(len(temp)):
             books.append(json_data[temp[idx]])
-        
-        
+            
+            
         books2 = []
         search_word = search_word.casefold()
         for word in json_data:
@@ -75,14 +76,13 @@ def search():
                 books2.append([word, "Based on Author"])     
             elif search_word in word['subjects'].casefold():
                 books2.append([word, "Baesd on Subject"])
-            
         
     return render_template('book/results.html', books=books)
 
 @bp.route('/bookshelves/', methods=('GET', 'POST'))
 def bookshelves():
-    
     model = Doc2Vec.load("gu.model")
+
     user_id = session.get('user_id')
     if user_id is None:
         return redirect(url_for('auth.login'))
@@ -94,7 +94,7 @@ def bookshelves():
             continue
         else:
             books.append(json_data[book.bookid])
-            
+    
     def recommend_full_text(text):
         sims = model.dv.most_similar(model.dv[text])
         return sims
@@ -120,8 +120,8 @@ def bookshelves():
     # 개수는 나중에 지정 --> 반복 횟수만큼 캐러셀에 책 추가
     for idx in related_book_title:
         related_book.append(json_data_key[idx[0]])
-        
-    return render_template('book/bookshelves.html', books=books, related_book = related_book)
+            
+    return render_template('book/bookshelves.html', books=books)
 
 @bp.route("/addToBookshelves")
 @login_required
