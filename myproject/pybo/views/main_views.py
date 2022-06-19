23d36f7448f0136ed61c2e9a4c873d7fe363d927
DESKTOP-RHQTACD\User <philoleben@gmail.com>
2022-06-19 11:16:49 +0900

    merge 에러로 인한 커밋 (?)

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -19,9 +19,51 @@ def index():
         unique_list = [x for x in seq if x not in seen and not seen.append(x)]
         return len(seq) == len(unique_list)
     
-    temp = []
-    books_for_authors = []
+    # 랜덤 저자 추출
+    def get_random_authors():
+        while True:
+           idx = random.randint(0, len(json_data) - 1)
+           author = json_data[idx]['authors'] 
+           count = 0
+           for title in json_data:
+               if title['authors'] == author:
+                   count += 1
+            
+           if count >= 3:
+               break;       
+        return author 
+
+    # 랜덤 subject 추출
+    def get_random_subjects():
+        while True:
+           idx = random.randint(0, len(json_data) - 1)
+           select_subject = json_data[idx]['subjects'][random.randint(0, len(json_data[idx]['subjects']) - 1)]
+           
+           subject_list = []
+           count = 0
+           for sub_list in json_data:
+                if select_subject in sub_list['subjects']:
+                    count += 1
+            
+           if count >= 3:
+               break;  
+        
+        
+        return select_subject
     
+    # 입력받은 데이터를 기반으로 검색하는 기능
+    def recommend_title(text):
+        new_vector = model.infer_vector(word_tokenize(text))
+        sims = model.dv.most_similar([new_vector])
+        return sims
+
+    # 책 제목을 기반으로 유사도 측정
+    def recommend_full_text(text):
+        sims = model.dv.most_similar(model.dv[text])
+        return sims
+    
+#-------------------------- json 파일 및 데이터 load ---------------------    
+
     model = Doc2Vec.load("C:\\gu.model")
     
     f = open('C:\\키없는거.json', 'r', encoding='UTF-8')
@@ -29,10 +71,7 @@ def index():
     
     q = open('quote.json', 'r', encoding= "UTF-8")
     quote = json.load(q, strict=False)
-    
-    # f2 = open('C:\\키 있는거.json', 'r', encoding="UTF-8")
-    # json_data2 = json.load(f2, strict=False)
-    
+      
     # book_id 값을 db에 저장
     # for book in json_data:   
 
@@ -44,48 +83,59 @@ def index():
     # db.session.query(Book).delete()
     # db.session.commit()
     
-    def recommend_title(text):
-        new_vector = model.infer_vector(word_tokenize(text))
-        sims = model.dv.most_similar([new_vector])
-        return sims
-
-    def recommend_full_text(text):
-        sims = model.dv.most_similar(model.dv[text])
-        return sims
+#---------------------------작가 랜덤으로 선택 후 데이터 가져오기-------------------------- 
+    
+    temp = []
+    books_for_authors = []
     
     vote_data = []
+    author = get_random_authors()
+    
     for book in json_data:
-        if book['authors'] == 'Plato':
+        if book['authors'] == author:
             data = Book.query.filter_by(bookid=book['id']).first()
             book["voter"] = len(data.voter)
             books_for_authors.append(book)
     # print(books_for_authors)
         
+#-----------------------------------download count 내림차순 정렬-----------------------------------------------    
+    
     books2 = []
-    a = sorted(json_data, key = lambda x: x['download_count'], reverse=True)
+    download_desc = sorted(json_data, key = lambda x: x['download_count'], reverse=True)
     
     for i in range(10):
-        books2.append(a[i])
+        books2.append(download_desc[i])
+
+#-----------------------------------------랜덤 subject --------------------------------------
+
+    subject1 = get_random_subjects()
+    subject2 = get_random_subjects()
+    while subject1 == subject2:
+        subject2 = get_random_subjects()
     
-    books3 = []
+    books3 = [subject1]
+    for book in json_data:
+        if subject1 in book['subjects']:
+            books3.append(book)
     
+    books4 = [subject2]
+    for book in json_data:
+        if subject2 in book['subjects']:
+            books4.append(book)
+            
+#--------------------------------------------------------------------------------------------------
+                                   
     lists = recommend_full_text('Aristotle on the art of poetry')
     lists.pop(0)
 
     # 키 없는거 사용 
-    for book in json_data:
-        for t in lists:
-            if book['title'] == t[0]:
-                books3.append(book)
+
     
-    """
-    키 있는거 
-    for book in lists:
-        books3.append(json_data2[book])
-    
-    """
+
     # print(json.dumps(books_for_authors, indent="\t") )
     
+#----------------------------------------인용구 추출 ------------------------------------------------
+  
     quote_list = []
     idx = []
     
@@ -103,9 +153,12 @@ def index():
     
     for i in range(5):
         quote_list.append([quote_json[idx[i][0]]['name'], quote_json[idx[i][0]]['photo'], quote_json[idx[i][0]]['ideas'][idx[i][1]]])
-    
-    return render_template('/main/index.html', books=books_for_authors, books2=books2, books3=books3, quote_list=quote_list)
 
+#--------------------------------------------------전달---------------------------------------------------------- 
+    
+    return render_template('/main/index.html', books=books_for_authors, books2=books2,  books3 = books3, books4 = books4, quote_list=quote_list)
+
+#---------------------------------------------------------------------------------------------------------------------
 @bp.route('/about')
 def about():
     return render_template('/main/about.html')
